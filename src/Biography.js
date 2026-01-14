fb9dbbc4d3dde11324c74dd25270dc7833bf111f
EltonCCL <lccechai@gmail.com>
2026-01-14 21:54:38 +0800

    fix: Update biography details and improve link security

--- src/Biography.js
+++ src/Biography.js
@@ -25,10 +25,10 @@ function Biography() {
       <Separate></Separate>
       <div style={{margin: '8px 0px'}}>
       <p>
-      I am an MPhil student at the <a href='https://cse.hkust.edu.hk' target='_blank'>Department of Computer Science and Engineering</a>, <a href='https://hkust.edu.hk' target='_blank'>Hong Kong University of Science and Technology (HKUST)</a>, and a member of the <a href='https://cse.hkust.edu.hk/dsf/DSF.html' target='_blank'>DSF Lab</a>. My supervisor is <a href='https://sites.google.com/view/xiaofang-zhou' target='_blank'>Prof. Xiaofang Zhou</a>. Previously, I interned at HKUST as a Junior Research Assistant and Dymon Asia Capital as a Quant Research Intern. I received my Bachelor's degree in Computer Science from HKUST in June 2024.
+      I am an MPhil student at the <a href='https://cse.hkust.edu.hk' target='_blank' rel='noopener noreferrer'>Department of Computer Science and Engineering</a>, <a href='https://hkust.edu.hk' target='_blank' rel='noopener noreferrer'>Hong Kong University of Science and Technology (HKUST)</a>, and a member of the <a href='https://cse.hkust.edu.hk/dsf/DSF.html' target='_blank' rel='noopener noreferrer'>DSF Lab</a>. My supervisor is <a href='https://sites.google.com/view/xiaofang-zhou' target='_blank' rel='noopener noreferrer'>Prof. Xiaofang Zhou</a>. I received my Bachelor's degree in Computer Science from HKUST in June 2024. Previously, I interned at J.P. Morgan as an Applied AI/ML Researcher, HKUST as a Junior Research Assistant, and Dymon Asia Capital as a Quant Research Intern.
       </p>
       <p>
-      Currently, I am focusing on research in routing optimization, reinforcement learning, multi-agent system. I am also interested in natural language processing (NLP) and large language model (LLM).</p>
+      My research focuses on routing optimization, reinforcement learning, and multi-agent systems. I am also interested in natural language processing (NLP) and large language models (LLMs), with applications in both research and industry contexts.</p>
       </div>
     </AboutMeContainer>
   );
